a3059da994177d94b3b28b652551d6fc5890c57d
Nghi Dang <dangquocnghi@gmail.com>
2025-10-05 16:46:57 +0700

    Generate enrollments blocks

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { CoursesModule } from './courses/courses.module';
+import { EnrollmentsModule } from './enrollments/enrollments.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, CoursesModule],
+  imports: [UsersModule, AuthModule, CoursesModule, EnrollmentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
