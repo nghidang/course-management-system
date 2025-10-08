98ee1b1afd6e8c7b9d85bb4be20f7489a4f5a0bf
Nghi Dang <dangquocnghi@gmail.com>
2025-10-08 15:33:17 +0700

    E2E test for register + login

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
 
@@ -21,5 +21,24 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  it('/auth/register (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/auth/register')
+      .send({ email: 'test@student.com', password: 'pass', role: 'Student' })
+      .expect(201);
+  });
+
+  it('/auth/login (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/auth/login')
+      .send({ email: 'test@student.com', password: 'pass' })
+      .expect(201)
+      .then((response) => {
+        const body: unknown = response.body;
+        const token = (body as { token?: unknown })?.token;
+        expect(typeof token === 'string' && token.length > 0).toBe(true);
+      });
   });
 });
